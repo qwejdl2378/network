d1e7cc89c60c6b4585ed4e08254a4d66c1830923
jidonglei <jidl@tuya.com>
2021-11-19 10:05:38 +0800

    feat: update opkg package

---
+++ openwrt.install.sh
@@ -0,0 +1,3 @@
+opkg update && opkg install luci luci-base iptables dnsmasq-full coreutils coreutils-nohup bash curl jsonfilter ca-certificates ipset ip-full iptables-mod-tproxy kmod-tun luci-compat
+
+
